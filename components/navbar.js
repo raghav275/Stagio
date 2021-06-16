942ecee88be9a96534e13e3c6d57a6f0a241ba7f
Pranav <pranav.134.pg@gmail.com>
2021-06-16 23:31:13 +0530

    Register Button

--- components/navbar.js
+++ components/navbar.js
@@ -11,11 +11,13 @@ const mainStyle = {
 };
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState("");
   const handleClose = () => {
     setOpen(false);
   };
-  const handleOpen = () => {
+  const handleOpen = (str) => {
     setOpen(true);
+    setSelected(str);
   };
   return (
     <div style={mainStyle}>
@@ -61,11 +63,26 @@ const Navbar = () => {
             fontFamily: "Poppins-Medium",
             borderColor: "#d94b58",
             color: "#ffffff",
+            marginRight: 20,
           }}
-          onClick={handleOpen}
+          onClick={() => handleOpen("Sign In")}
           variant="outline-primary"
         >
           Sign in
+        </Button>
+        <Button
+          className={styles.sign}
+          style={{
+            borderRadius: "50px",
+            borderWidth: 2,
+            fontFamily: "Poppins-Medium",
+            borderColor: "#d94b58",
+            color: "#ffffff",
+          }}
+          onClick={() => handleOpen("Register")}
+          variant="outline-primary"
+        >
+          Register
         </Button>
         <Modal
           show={open}
@@ -84,10 +101,30 @@ const Navbar = () => {
               style={{ color: "#d94b58" }}
               id="contained-modal-title-vcenter"
             >
-              Sign In
+              {selected}
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
+            {selected==="Register" && 
+            <div>
+              <p style={{ marginBottom: 0, padding: 10, color: "#d94b58" }}>
+              Enter Your Full Name
+            </p>
+            <input
+              style={{
+                width: "100%",
+                padding: 10,
+                marginBottom: 10,
+                borderRadius: 60,
+                border: "2px solid #d94b58",
+                background: "transparent",
+                color: "#ffffff",
+                outline: "none",
+              }}
+              placeholder="Enter Here"
+            ></input>
+            </div>
+            }
             <p style={{ marginBottom: 0, padding: 10, color: "#d94b58" }}>
               Enter Your Email
             </p>
@@ -104,6 +141,25 @@ const Navbar = () => {
               }}
               placeholder="Enter Here"
             ></input>
+              {selected!== "Forgot Password"&&
+              <div>
+                <p style={{ marginBottom: 0, padding: 10, color: "#d94b58" }}>
+              Enter Your Password
+            </p>
+            <input
+              style={{
+                width: "100%",
+                padding: 10,
+                marginBottom: 10,
+                borderRadius: 60,
+                border: "2px solid #d94b58",
+                background: "transparent",
+                color: "#ffffff",
+                outline: "none",
+              }}
+              placeholder="Enter Here"
+            ></input>
+            </div>}
           </Modal.Body>
           <Modal.Footer style={{ justifyContent: "center", border: "none" }}>
             <Button
@@ -113,8 +169,33 @@ const Navbar = () => {
                 borderRadius: 20,
               }}
             >
-              Sign In
+              {selected}
             </Button>
+            { selected === "Sign In" && 
+              <div style={{marginLeft: 10}}>
+                <Button
+                style={{
+                border: "none",
+                backgroundColor: "#d94b58",
+                borderRadius: 20,
+              }}
+              onClick={() => handleOpen("Register")}
+            >
+              New User?
+            </Button>
+            <Button
+                style={{
+                border: "none",
+                backgroundColor: "#d94b58",
+                borderRadius: 20,
+                marginLeft: 10
+              }}
+              onClick={() => handleOpen("Forgot Password")}
+            >
+              Forgot Password
+            </Button>
+              </div>
+            }
           </Modal.Footer>
         </Modal>
       </div>
