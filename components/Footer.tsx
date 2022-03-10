338270cd6a195b3f9d43e6f9dedbce1105326d69
Raghav Gupta <>
2022-03-11 00:37:27 +0530

    added privacy policy and t&c

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import TwitterIcon from "@material-ui/icons/Twitter";
+import Modal from "react-bootstrap/Modal";
+import { useStore } from "react-redux";
+import PrivacyPolicy from "./privacyPolicy";
+import TermsConditions from "./TermsConditions";
+import { css } from "@emotion/css";
 
 const Footer = () => {
+  const [isOpen, setOpen] = useState(false);
+  const [flag, setFlag] = useState(0);
   return (
     <div
       style={{
@@ -52,10 +59,59 @@ const Footer = () => {
           width: "30%",
         }}
       >
-        <p>Privacy Policy</p>
-        <p>Terms and Conditions</p>
+        <p
+          onClick={() => {
+            setOpen(true);
+            setFlag(1);
+          }}
+        >
+          Privacy Policy
+        </p>
+        <p
+          onClick={() => {
+            setOpen(true);
+            setFlag(2);
+          }}
+        >
+          Terms and Conditions
+        </p>
         <p>FAQs</p>
       </div>
+      <Modal
+        show={isOpen}
+        onHide={() => setOpen(false)}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        dialogClassName={css({
+          width: "50%",
+        })}
+        contentClassName={css({
+          backgroundColor: "#ffffff",
+          borderRadius: 20,
+        })}
+      >
+        <Modal.Header closeButton style={{ border: "none", color: "#d94b58" }}>
+          <Modal.Title
+            style={{ color: "#000000", marginLeft: 10 }}
+            id="contained-modal-title-vcenter"
+          >
+            {flag === 1 ? "Privacy Policy" : "Terms and Conditions"}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div
+            className={css({
+              display: "flex",
+              overflow: "auto",
+              maxHeight: "70vh",
+              padding: 15,
+            })}
+          >
+            {flag === 1 ? <PrivacyPolicy /> : <TermsConditions />}
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
